a7e7d771fe7664fc31c898b7f29508c5ab2e324b
Willy Stadnick <willy.stadnick@gmail.com>
2020-06-27 19:21:09 -0300

    Add main

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+// https://www.calhoun.io/creating-random-strings-in-go
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+	"0123456789" +
+	"-_"
+
+var randSeed *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func RandStringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[randSeed.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func RandString(length int) string {
+	return RandStringWithCharset(length, charset)
+}
+
+func main() {
+	for {
+		url := "https://hackmd.io/" + RandString(22)
+		response, err := http.Get(url)
+		if err != nil {
+			log.Fatalf("error on get url %s: %v", url, err)
+		}
+		mark := ""
+		if response.StatusCode == 200 {
+			mark = "<<<"
+		}
+		log.Println(url, response.StatusCode, mark)
+	}
+}
